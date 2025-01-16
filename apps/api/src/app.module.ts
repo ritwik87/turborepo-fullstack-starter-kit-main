7eb3f3f27e5f7a1385e754f73665c03011bd0dcb
techovaryadev <ritwikmukhopadhyay@gmail.com>
2025-01-16 18:12:22 +0530

    Changes

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../../web', '.next'),
+      exclude: ['api/*'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
